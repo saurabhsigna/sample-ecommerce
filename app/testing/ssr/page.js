dff67b27e056cf58777a7d5f957ea37f91c9fdd9
saurabh pal <131267391+saurabhsigna@users.noreply.github.com>
2023-07-18 01:51:16 +0000

    fix(ssr): fix fetch request and response handling

--- app/testing/ssr/page.js
+++ app/testing/ssr/page.js
@@ -5,18 +5,20 @@ export default async function App({ params: { label } }) {
   let isErrorPresent = false;
   async function getProducts() {
     try {
-      const res = await fetch("https://m0wg74-3000.csb.app", {
+      const body = JSON.stringify({ bookCategory: label });
+      const res = await fetch("https://m0wg74-3000.csb.app/fetchproducts", {
         method: "GET",
         headers: {
           "Content-Type": "application/json", // Ensure correct content type
         },
+        body,
       });
 
       if (!res.ok) {
         isErrorPresent = true;
       }
 
-      return res.text();
+      return res.json();
     } catch (error) {
       isErrorPresent = true;
       throw error;
