4d77ea5d73719a6610ad65ef7643dbf881c35a1a
saurabh pal <131267391+saurabhsigna@users.noreply.github.com>
2023-07-18 09:50:09 +0000

    refactor(config): refactor next.config.js to enable experimental runtime

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-swcMinify:true
+  experimental: {
+    runtime: "edge",
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
